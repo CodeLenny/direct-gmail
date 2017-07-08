59593a5e69347e416e0b4980b054cd9d6859d66f
Ryan Leonard <rhinoisnot@gmail.com>
2017-07-08 17:25:38 -0400

    Built script.

---
+++ direct-gmail.meta.js
@@ -0,0 +1,18 @@
+
+// ==UserScript==
+// @name         Direct GMail Link
+// @namespace    http://codelenny.com/
+// @version      0.0000000000
+// @description  Direct link to Gmail
+// @author       Ryan Leonard (http://ryanleonard.us)
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.10/URI.js
+// @grant        GM_registerMenuCommand
+// @grant        GM_setClipboard
+// @grant        GM_notification
+// @match        https://gmail.com/*
+// @match        https://mail.google.com/*
+// @updateURL    https://raw.githubusercontent.com/CodeLenny/direct-gmail/master/direct-gmail.meta.js
+// @downloadURL  https://raw.githubusercontent.com/CodeLenny/direct-gmail/master/direct-gmail.user.js
+// ==/UserScript==
